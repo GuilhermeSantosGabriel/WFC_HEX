9df690e06ae55fc4fe80c2b724d9942c5c00db45
gui27souza <guilhermeoliveiradesouza@gmail.com>
2026-05-02 21:16:38 -0300

    feat - updates Cell implementation, adds (x,y) coords for performance

--- WFC_Core/include/models/cell.h
+++ WFC_Core/include/models/cell.h
@@ -9,11 +9,17 @@ public:
     int entropy;
     std::set<int> possible_tiles;
     bool collapsed;
+    double x, y;
 
     // Constructor
-    Cell(int q, int r, int s, int height,
-         int e,
-         const std::set<int>& t);
+    Cell(int q, int r, int s,
+        double x_coord, double y_coord,
+        int height,
+        int e, const std::set<int>& t);
+
+    void update_possible_tiles();
+
+    void collapse();
 };
 
 #endif
--- WFC_Core/src/models/cell.cpp
+++ WFC_Core/src/models/cell.cpp
@@ -1,6 +1,23 @@
 #include "models/cell.h"
+#include "engine/hex_to_pixels.h"
 
-Cell::Cell(int q, int r, int s, int height,
-           int e,
-           const std::set<int>& t)
-    : Hex(q, r, s, height), entropy(e), possible_tiles(t), collapsed(false) {}
+Cell::Cell(
+    int q, int r, int s,
+    double x_coord, double y_coord,
+    int height,
+    int e, const std::set<int>& t)
+:
+    Hex(q, r, s, height),
+    x(x_coord), y(y_coord),
+    entropy(e), possible_tiles(t), collapsed(false) {}
+
+
+// TODO - Cell - update_possible_tiles()
+void Cell::update_possible_tiles() {
+
+}
+
+// TODO - Cell - collapse()
+void Cell::collapse() {
+
+}
