80b186cf837b37f7fc80646b41d2901524f0159a
guilherme-souza_avenue <205801399+guilherme-souza_avenue@users.noreply.github.com>
2026-05-10 19:08:05 -0300

    feat - implements MacOS compatibility

--- WFC_Core/src/engine/opengl/window_manager.cpp
+++ WFC_Core/src/engine/opengl/window_manager.cpp
@@ -17,6 +17,10 @@ GLFWwindow* setup_window() {
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+    #ifdef __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #endif
 
     GLFWwindow* window = glfwCreateWindow(1000, 1000, "WFC_HEX", NULL, NULL);
     if (window == NULL) {
