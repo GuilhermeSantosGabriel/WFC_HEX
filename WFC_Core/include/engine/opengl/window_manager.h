299530af641a9e7cf00e452347778e8569fbb232
gui27souza <guilhermeoliveiradesouza@gmail.com>
2026-05-10 16:29:38 -0300

    feat - implements window manager module

--- WFC_Core/include/engine/opengl/window_manager.h
+++ WFC_Core/include/engine/opengl/window_manager.h
@@ -1,1 +1,13 @@
+#ifndef WINDOW_MANAGER_H
+#define WINDOW_MANAGER_H
 
+#include "models/hexmap.h"
+
+#include <external/glad/glad.h>
+#include <external/GLFW/glfw3.h>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+
+GLFWwindow* setup_window();
+
+#endif
